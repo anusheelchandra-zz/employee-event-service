516fea742dae637a9c11320cf8ae58d001feab01
Anusheel Chandra <anusheel.chandra@goeuro.com>
2020-08-22 20:44:41 +0200

    chore : updated run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,21 @@
+
+sh ./stop_app.sh
+
 echo "pulling event-service from docker hub"
 docker pull anusheelchandra/event-service
 
 echo "pulling employee-service from docker hub"
 docker pull anusheelchandra/employee-service
 
-echo "starting the applciation .PLesse wait ......"
+echo "starting the applciation .Plesse wait ......"
 docker-compose up -d
+
+cnt=`docker ps | grep event-service_employee-service | awk '{print $1}'`
+d=`docker logs $cnt| grep "Started EmployeeServiceApplication"|wc -l`
+echo "waiting for application to start in 3 secs .."
+sleep 1
+echo "waiting for application to start in 2 secs .."
+sleep 1
+echo "waiting for application to start in 1 secs .."
+sleep 1
+
