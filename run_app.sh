9861b12809fbd2ea78b26612b4e02040b7f11814
Anusheel Chandra <anusheel.chandra@goeuro.com>
2020-08-23 19:00:35 +0200

    chore : updated run_app.sh file

--- run_app.sh
+++ run_app.sh
@@ -1,19 +1,14 @@
 
 sh ./stop_app.sh
 
-echo "pulling event-service from docker hub"
-docker pull anusheelchandra/event-service
-
-echo "pulling employee-service from docker hub"
-docker pull anusheelchandra/employee-service
-
 echo "starting the applciation .Plesse wait ......"
 docker-compose up -d
 
+echo "waiting for application to start in 30 secs .."
+sleep 30
 echo "waiting for application to start in 10 secs .."
 sleep 10
 echo "waiting for application to start in 5 secs .."
 sleep 5
-echo "waiting for application to start in 1 secs .."
-sleep 1
+echo "Please check logs if you cannot access the service .."
 
